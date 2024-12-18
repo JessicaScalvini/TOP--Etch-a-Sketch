94bdc98a0d12d1a93d00059515a364e63448bd83
JessicaScalvini <jessica.scalvini98@gmail.com>
2024-12-18 21:52:10 +0100

    da finire...

---
+++ synth.js
@@ -0,0 +1,12 @@
+const container = document.querySelector('.container');
+let Num= 16;
+
+for (let i=0; i<=Num; i++){
+    const div = document.createElement('div');
+    container.appendChild(div);
+}
+
+
+
+
+
